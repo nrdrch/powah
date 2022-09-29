0d971d656fcd303cb2040ca8f47a0dea1b9c6f92
Lukas aka sticky aka MoeJoe <96546774+nrdrch@users.noreply.github.com>
2022-09-29 14:05:41 +0200

    Create MSpr.ps1

---
+++ MSpr.ps1
@@ -0,0 +1,7 @@
+oh-my-posh init pwsh | Invoke-Expression
+oh-my-posh init pwsh --config "$HOME\Documents\WindowsPowerShe
+Import-Module "$HOME\Documents\WindowsPowerShell\modules.psm1"
+
+New-Alias -Name gh -Value Get-Help
+New-Alias -Name c -Value clear
+New-Alias -Name powah -Value Edit-CurrentOMPTheme
