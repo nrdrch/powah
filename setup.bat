e4ead7148dd533d4eaf4f49c83b7b3c22cf30850
Lukas aka sticky aka MoeJoe <96546774+nrdrch@users.noreply.github.com>
2022-09-29 15:19:00 +0200

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -18,10 +18,6 @@ IF NOT EXIST custom.omp.json (
 
 ) 
 
-mkdir %USERPROFILE%\Documents\fonts
-mkdir %USERPROFILE%\Documents\fonts\Meslo
-powershell -Command "wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip -O $HOME\Documents\Melso.zip"
-powershell -Command "tar -xf $HOME\Documents\Meslo.zip -C $HOME\Documents\fonts\Meslo"
 
 powershell -Command ". $PROFILE; Clear-Host"
 
