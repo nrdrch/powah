1118d39bf8264441c2dbf8a368b817abd1cc6bec
Lukas aka sticky aka MoeJoe <96546774+nrdrch@users.noreply.github.com>
2022-10-04 17:02:38 +0200

    Add files via upload

--- powah.bat
+++ powah.bat
@@ -1,8 +1,28 @@
 @ECHO OFF
 title powah
 CLS 
+set "working=%~dp0"
+set "pshome=C:\Users\%username%\Documents\WindowsPowerShell"
+
+IF NOT EXIST %pshome%\ds (
+    mkdir "%pshome%\ds"
+)
+
+
+IF NOT EXIST %pshome%\ds\popeye.txt (
+    copy /y %working%\popeye.txt C:\Users\%username%\Documents\WindowsPowerShell\ds\popeye.txt
+)
 
 SET "pop= C:\Users\%username%\Documents\WindowsPowerShell\ds\popeye.txt"
+cd %pshome%\ds
+
+IF NOT EXIST %pshome%\ds\custom.omp.json (
+    powershell.exe -Command "New-Item custom.omp.json -Type File -Force"
+)
+
+
+
+
 
 CLS
 :MENU
@@ -27,7 +47,7 @@ powershell.exe -Command "Clear-Host"
 
 set /p theme= Enter The Theme Name: 
 copy /y C:\Users\%username%\AppData\Local\Programs\oh-my-posh\themes\%theme%.omp.json C:\Users\%username%\Documents\WindowsPowerShell\ds\custom.omp.json
-powershell.exe -Command [void]"{. $PROFILE}"
+powershell.exe -Command [void]"{. $PROFILE; Clear-Host}"
 
 GOTO Quit
 
