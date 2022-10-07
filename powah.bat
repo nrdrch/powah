fbbd22ac93e8cb1da0e43e0da81d3a0fdd078a95
Lukas aka sticky aka MoeJoe <96546774+nrdrch@users.noreply.github.com>
2022-10-07 23:22:42 +0200

    Add files via upload

--- powah.bat
+++ powah.bat
@@ -3,7 +3,6 @@ title powah
 CLS 
 
 SET "pop= C:\Users\%username%\Documents\WindowsPowerShell\ds\popeye.txt"
-
 SET "thms= C:\Users\%username%\Documents\WindowsPowerShell\ds\allposhthemes.txt"
 
 
@@ -16,16 +15,15 @@ Type %pop%
 SET INPUT=
 SET /P INPUT= Choose An Option (Or Q To Quit):  
 
-
 IF /I '%INPUT%'=='1' GOTO themebyname
 IF /I '%INPUT%'=='2' GOTO themesonline
 IF /I '%INPUT%'=='3' GOTO upthemes
+IF /I '%INPUT%'=='4' GOTO uppowah
 IF /I '%INPUT%'=='Q' GOTO Quit
+
+
 CLS
 GOTO MENU
-
-
-
 :themebyname
 CLS
 Type %thms%
@@ -37,59 +35,68 @@ IF /I '%theme%'=='glowsticks' GOTO exception
 IF /I '%theme%'=='devious-diamonds' GOTO exception
 
 GOTO usual
-
 :usual
+
 DEL C:\Users\%username%\Documents\WindowsPowerShell\ds\custom*
 copy /y C:\Users\%username%\AppData\Local\Programs\oh-my-posh\themes\%theme%.omp.json C:\Users\%username%\Documents\WindowsPowerShell\ds\custom.omp.json
 
 GOTO Reload
-
 :exception
+
 DEL C:\Users\%username%\Documents\WindowsPowerShell\ds\custom*
 copy /y C:\Users\%username%\AppData\Local\Programs\oh-my-posh\themes\%theme%.omp.yaml C:\Users\%username%\Documents\WindowsPowerShell\ds\custom.omp.yaml
 
 GOTO Reload
-
 :themesonline
 start https://ohmyposh.dev/docs/themes
 
 
 GOTO Quit
-
 :upthemes
+
 SET "thmdir= C:\Users\%username%\AppData\Local\Programs\oh-my-posh\themes"
 cd %TEMP%
+IF EXIST %TEMP%\oh-my-posh (
+    RMDIR /S /Q %TEMP%\oh-my-posh
+)
 git clone https://github.com/JanDeDobbeleer/oh-my-posh.git
-xcopy /s /q %TEMP%\oh-my-posh\themes %thmdir%\ 
+xcopy /s /q /y %TEMP%\oh-my-posh\themes %thmdir%\ 
 
 
 powershell -Command "Get-ChildItem -Path $HOME\AppData\Local\Programs\oh-my-posh\themes | Select-Object -ExpandProperty Name | Out-File $HOME\AppData\Local\Temp\pthemp.txt"
 
-
+:: set "upnmls= powershell -Command "Get-ChildItem -Path $HOME\AppData\Local\Programs\oh-my-posh\themes | Select-Object -ExpandProperty Name | Out-File $HOME\AppData\Local\Temp\pthemp.txt"
 :: Type %TEMP%\pthemp.txt > C:\Users\%username%\Documents\WindowsPowerShell\ds\allposhthemes.txt
-
-
-
-
-
 cls
+
 GOTO MENU
-
-
-
 :Reload
 
 
-
-
 powershell -Command [void]"{. $PROFILE; Clear-Host}"
-
 GOTO Quit
+
+
+
+
+
+
+:uppowah
+
+SET "powahdir= C:\Users\%username%\Documents\WindowsPowerShell"
+cd %TEMP%
+IF EXIST %TEMP%\powah (
+    RMDIR /S /Q %TEMP%\powah
+)
+git clone https://github.com/nrdrch/powah.git
+
+
+copy /y %TEMP%\powah\powah.bat %powahdir%\powah.bat
 
 :Quit
 cls
 
-ECHO If your theme did not load, manually Reload your PowerShellProfile
+ECHO If Your Theme DID NOT Load, Manually Reload Your PowerShellProfile. (e.g.: . $PROFILE)
 
 
 
