76e1d1c582060f41779c2f28db686a3f4ddce802
Lukas aka sticky aka MoeJoe <96546774+nrdrch@users.noreply.github.com>
2022-10-08 18:36:06 +0200

    Update PowerShellProfileExample.ps1

--- PowerShellProfileExample.ps1
+++ PowerShellProfileExample.ps1
@@ -1,5 +1,3 @@
 oh-my-posh init pwsh --config "$HOME\Documents\WindowsPowerShell\ds\custom*" | Invoke-Expression
-Import-Module $HOME\Documents\WindowsPowerShell\pwhmodule.psm1
-Import-Module PSReadLine
-Import-Module Terminal-Icons
+Import-Module $HOME\Documents\WindowsPowerShell\ds\pwhmodule.psm1
 New-Alias -Name powah -Value Edit-CurrentOMPTheme
