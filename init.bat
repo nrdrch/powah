f489546b45b67deb034b6ce3d504e7237a8022a6
Lukas aka sticky aka MoeJoe <96546774+nrdrch@users.noreply.github.com>
2022-10-15 09:21:25 +0200

    Add files via upload

--- init.bat
+++ init.bat
@@ -1,7 +1,6 @@
 @ECHO OFF
-SET pwhpth=%~dp0
-echo %pwhpth:~0,-1%
-
+SET currentpath=%~dp0
+echo %currentpath:~0,-1%
 IF NOT EXIST C:\Users\%username%\AppData\Local\Programs\oh-my-posh\bin\oh-my-posh.exe (
     powershell -Command "Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))"
 )
@@ -11,33 +10,40 @@ IF EXIST C:\Users\%username%\Documents\WindowsPowerShell\Microsoft.PowerShell_pr
 IF NOT EXIST C:\Users\%username%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 (
     GOTO ifpsprofildsntexists
 )
-
-
-
+:install
+cls
+IF NOT EXIST %temp%\powahds (
+    GOTO install
+)
+IF EXIST %temp%\powahds (
+    RMDIR %temp%\powahds; GOTO install
+)
+cd %temp%
+git clone https://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@github.com/nrdrch/powahds.git %temp%
+copy /y %currentpath%\powah.bat C:\Users\%username%\Documents\WindowsPowerShell\powah.bat
+IF NOT EXIST C:\Users\%username%\Documents\WindowsPowerShell\ds (
+    MKDIR C:\Users\%username%\Documents\WindowsPowerShell\ds
+)
+xcopy /s /q /y %temp%\powahds C:\Users\%username%\Documents\WindowsPowerShell\ds
 :ifpsprofilexists
 cls
 SET "psplctn= C:\Users\%username%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
 powershell -Command "Get-Content $PROFILE | Select-String -NotMatch 'oh-my-posh init pwsh *' | Out-File -FilePath $HOME\AppData\Local\Temp\psprofiledump.txt"
 powershell -Command "Get-Content $HOME\AppData\Local\Temp\psprofiledump.txt | Select-String -NotMatch 'New-Alias -Name powah *' | Out-File -FilePath $HOME\AppData\Local\Temp\psprofiledump2.txt"
-type %pwhpth%\PowerShellProfileExample.ps1 >> %temp%\Microsoft.PowerShell_profile.ps1 
+type %currentpath%\PowerShellProfileExample.ps1 >> %temp%\Microsoft.PowerShell_profile.ps1 
 DEL %psplctn%
-
 copy /y %temp%\Microsoft.PowerShell_profile.ps1 %psplctn%
 type %temp%\psprofiledump2.txt >> %psplctn%
-
-GOTO nextup
+GOTO install
 :ifpsprofildsntexists
 cls
-copy /y %pwhpth%\PowerShellProfileExample.ps1 C:\Users\%username%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+IF NOT EXIST C:\Users\%username%\Documents\WindowsPowerShell (
+    MKDIR C:\Users\%username%\Documents\WindowsPowerShell
+)
+copy /y %currentpath%\PowerShellProfileExample.ps1 C:\Users\%username%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
 SET "psplctn= C:\Users\%username%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
-copy /y %pwhpth%\PowerShellProfileExample.ps1 C:\Users\%username%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
-GOTO nextup
-:nextup
-
-IF NOT EXIST C:\Users\%username%\Documents\WindowsPowerShell\ds (
-    xcopy /y /i %pwhpth%\ds C:\Users\%username%\Documents\WindowsPowerShell\ds
-) 
-
-copy /y %pwhpth%\powah.bat C:\Users\%username%\Documents\WindowsPowerShell\powah.bat
-GOTO Quit
+copy /y %currentpath%\PowerShellProfileExample.ps1 C:\Users\%username%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+GOTO install
 :Quit
+cls
+ECHO PLEASE RESTART POWERSHELL
