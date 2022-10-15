22a39d69969ed8a1dfa93b0794580d6071f06362
Lukas aka sticky aka MoeJoe <96546774+nrdrch@users.noreply.github.com>
2022-10-15 15:26:38 +0200

    Update init.bat

--- init.bat
+++ init.bat
@@ -1,9 +1,5 @@
 @ECHO OFF
 SET currentpath=%~dp0
-powershell -Command "$currentdir = Get-Location"
-IF EXIST %currentpath%\stp.ps1 (
-    powershell -Command "Set-ExecutionPolicy Bypass -Scope Process -Force stp.ps1"; DEL %currentpath%\stp.ps1 /q ; EXIT
-)
 IF NOT EXIST C:\Users\%username%\AppData\Local\Programs\oh-my-posh\bin\oh-my-posh.exe (
     powershell -Command "Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))"
 )
