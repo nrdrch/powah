c348f51e781c4b02cbd4dbcb9e1247faf9228252
Lukas aka sticky aka MoeJoe <96546774+nrdrch@users.noreply.github.com>
2022-10-28 17:26:08 +0200

    Update PowerShellProfileExample.ps1

--- PowerShellProfileExample.ps1
+++ PowerShellProfileExample.ps1
@@ -1,3 +1,4 @@
-oh-my-posh init pwsh --config "$HOME\Documents\WindowsPowerShell\ds\custom*" | Invoke-Expression
+oh-my-posh init pwsh --config "$HOME\Documents\WindowsPowerShell\ds\custom.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$HOME\Documents\WindowsPowerShell\ds\custom.omp.yaml" | Invoke-Expression
 Import-Module $HOME\Documents\WindowsPowerShell\ds\pwhmodule.psm1
 New-Alias -Name powah -Value Edit-CurrentOMPTheme
